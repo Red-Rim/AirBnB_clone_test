1a4ff2bf24637cdd56166b3c204b5d2624f950ca
ElRoccinante <alaechouiye@hotmail.com>
2023-10-15 00:23:51 +0200

    k

--- console.py
+++ console.py
@@ -5,14 +5,6 @@ Module of AIRBNB console
 
 import cmd
 from models.base_model import BaseModel
-from models.user import User
-from models.engine.file_storage import FileStorage
-from models import storage
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
 import shlex
 import sys
 import json
